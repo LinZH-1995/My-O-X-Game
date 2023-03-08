6c074105c6d29420ea0848528214e593db58a2d0
LinZH-1995 <lin84610@gmail.com>
2023-03-08 14:13:40 +0800

    optimize function & fix bug

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 const table = document.querySelector("table");
 const positionData = { o: [], x: [] };
 const winConditions = [...rows(), ...columns(), [1, 5, 9], [3, 5, 7]]
+let someoneWin = false
 
 // function
 function renderPlayer(position) {
-  const box = document.querySelector(`td[data-position="${position}"]`)
-  if (box === null || box.innerHTML) return
-  positionData['o'].push(position)
-  box.innerHTML = `<i class="fa-regular fa-o"></i>`
-  if (checkWhetherWin(positionData['o'])) {
-    setTimeout(() => {
-      alert(`Player: "O" Win !`)
-    }, 50);
+  if (draw(position, 'o')) {
+    if (checkWhetherWin(positionData['o'])) {
+      someoneWin = true
+      setTimeout(() => {
+        alert(`Player: "O" Win !`)
+      }, 50);
+    }
+    return true
   }
+  return false
 }
 
 function renderAI() {
   // findemptyposition
   const emptyPositions = findEmptyPosition()
-  console.log(emptyPositions)
   if (emptyPositions.length === 0) {
     return setTimeout(() => {
       alert('Tied !')
@@ -26,53 +27,48 @@ function renderAI() {
   }
   // checkwhetherwin
   const aiCopy = positionData['x'].slice()
-  for (const position of emptyPositions) {
-    aiCopy.push(position)
-    if (checkWhetherWin(aiCopy)) {
-      positionData['x'].push(position)
-      const box = document.querySelector(`td[data-position="${position}"]`)
-      box.innerHTML = `<i class="fa-solid fa-x"></i>`
-      console.log('hi')
-      return setTimeout(() => {alert(`Player: "X" Win !`)}, 50)
-    }
-    aiCopy.pop()
+  if (checkWinOrLoseForAI(emptyPositions, aiCopy)) {
+    someoneWin = true
+    return setTimeout(() => { alert(`Player: "X" Win !`) }, 50)
   }
   // checkwhetherlose
   const playerCopy = positionData['o'].slice()
-  for (const position of emptyPositions) {
-    playerCopy.push(position)
-    if (checkWhetherWin(playerCopy)) {
-      positionData['x'].push(position)
-      const box = document.querySelector(`td[data-position="${position}"]`)
-      box.innerHTML = `<i class="fa-solid fa-x"></i>`
-      return
-    }
-    playerCopy.pop()
-  }
+  if (checkWinOrLoseForAI(emptyPositions, playerCopy)) return
   // check "5" position
   if (emptyPositions.includes(5)) {
-    positionData['x'].push(5)
-    const box = document.querySelector('td[data-position="5"]')
-    box.innerHTML = `<i class="fa-solid fa-x"></i>`
-    return
+    return draw(5, 'x')
   }
   // check [1, 3, 7, 9] position
   const arr = [1, 3, 7, 9]
   for (const position of arr) {
     if (emptyPositions.includes(position)) {
-      positionData['x'].push(position)
-      const box = document.querySelector(`td[data-position="${position}"]`)
-      box.innerHTML = `<i class="fa-solid fa-x"></i>`
-      return
+      return draw(position, 'x')
     }
   }
   // random position
   const positionIndex = Math.floor(Math.random() * emptyPositions.length)
   const position = emptyPositions[positionIndex]
-  positionData['x'].push(position)
+  return draw(position, 'x')
+}
+
+function draw(position, who) {
   const box = document.querySelector(`td[data-position="${position}"]`)
-  box.innerHTML = `<i class="fa-solid fa-x"></i>`
-  return
+  if (box === null || box.innerHTML !== "") return false
+  positionData[who].push(position)
+  box.innerHTML = `<i class="fa-regular fa-${who}"></i>`
+  return true
+}
+
+function checkWinOrLoseForAI(emptyPositions, whosCopy) {
+  for (const position of emptyPositions) {
+    whosCopy.push(position)
+    if (checkWhetherWin(whosCopy)) {
+      draw(position, 'x')
+      return true
+    }
+    whosCopy.pop()
+  }
+  return false
 }
 
 function findEmptyPosition() {
@@ -117,9 +113,12 @@ function columns() {
 
 // addEventListener
 table.addEventListener("click", function (event) {
-  const position = Number(event.target.dataset.position);
-  renderPlayer(position);
-  setTimeout(() => {
-    renderAI()
-  }, 50);
+  const position = Number(event.target.dataset.position)
+  if (someoneWin) return
+
+  if (renderPlayer(position)) {
+    setTimeout(() => {
+      renderAI()
+    }, 50);
+  }
 });
