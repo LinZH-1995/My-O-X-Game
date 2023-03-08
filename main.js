7776a5819c22843a1909860b332f57d91a7519a9
LinZH-1995 <lin84610@gmail.com>
2023-03-08 13:20:16 +0800

    finish AI function

--- main.js
+++ main.js
@@ -17,16 +17,62 @@ function renderPlayer(position) {
 
 function renderAI() {
   // findemptyposition
-
+  const emptyPositions = findEmptyPosition()
+  console.log(emptyPositions)
+  if (emptyPositions.length === 0) {
+    return setTimeout(() => {
+      alert('Tied !')
+    }, 50);
+  }
   // checkwhetherwin
-
+  const aiCopy = positionData['x'].slice()
+  for (const position of emptyPositions) {
+    aiCopy.push(position)
+    if (checkWhetherWin(aiCopy)) {
+      positionData['x'].push(position)
+      const box = document.querySelector(`td[data-position="${position}"]`)
+      box.innerHTML = `<i class="fa-solid fa-x"></i>`
+      console.log('hi')
+      return setTimeout(() => {alert(`Player: "X" Win !`)}, 50)
+    }
+    aiCopy.pop()
+  }
   // checkwhetherlose
-  
+  const playerCopy = positionData['o'].slice()
+  for (const position of emptyPositions) {
+    playerCopy.push(position)
+    if (checkWhetherWin(playerCopy)) {
+      positionData['x'].push(position)
+      const box = document.querySelector(`td[data-position="${position}"]`)
+      box.innerHTML = `<i class="fa-solid fa-x"></i>`
+      return
+    }
+    playerCopy.pop()
+  }
   // check "5" position
-
+  if (emptyPositions.includes(5)) {
+    positionData['x'].push(5)
+    const box = document.querySelector('td[data-position="5"]')
+    box.innerHTML = `<i class="fa-solid fa-x"></i>`
+    return
+  }
   // check [1, 3, 7, 9] position
-
+  const arr = [1, 3, 7, 9]
+  for (const position of arr) {
+    if (emptyPositions.includes(position)) {
+      positionData['x'].push(position)
+      const box = document.querySelector(`td[data-position="${position}"]`)
+      box.innerHTML = `<i class="fa-solid fa-x"></i>`
+      return
+    }
+  }
   // random position
+  const positionIndex = Math.floor(Math.random() * emptyPositions.length)
+  const position = emptyPositions[positionIndex]
+  positionData['x'].push(position)
+  const box = document.querySelector(`td[data-position="${position}"]`)
+  box.innerHTML = `<i class="fa-solid fa-x"></i>`
+  return
 }
 
 function findEmptyPosition() {
@@ -39,7 +85,6 @@ function findEmptyPosition() {
 function checkWhetherWin(who) {
   for (const condition of winConditions) {
     if (condition.every(position => who.includes(position))) {
-      console.log('hi')
       return true
     }
   }
@@ -74,4 +119,7 @@ function columns() {
 table.addEventListener("click", function (event) {
   const position = Number(event.target.dataset.position);
   renderPlayer(position);
+  setTimeout(() => {
+    renderAI()
+  }, 50);
 });
